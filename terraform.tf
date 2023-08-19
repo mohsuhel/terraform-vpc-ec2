f455763189394cd8c244dc98a3cd5a97ae479745
Suhel <suhmd8530@gmail.com>
2023-08-19 14:19:09 +0530

    terraform assignment

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configuring provider
+provider "aws" {
+region = "us-east-2"
+access_key = "AKIA4S3DRRZQB3J7QL6O"
+secret_key = "WwlhkRvbzdXXdBlJ2zguXlW2uBEIy/FMEnBevt/r"
+}
+
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform hands-on"
+  }
+}
+
